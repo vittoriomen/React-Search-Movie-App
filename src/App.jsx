d0e6384cf2f7171da585ba914e8fcd246c49f87e
vittorio <vittoriomenicanti@hotmail.com>
2025-02-08 17:53:32 +0100

    set up react router and created Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/home";
+import Favorites from "./pages/Favorites";
+
 
 function App() {
  
 
-
   return (
-    <>
-      <Home/>
+    
+      <main className="main-content">
 
-    </>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Favorites" element={<Favorites/>}/>
+        </Routes>
+      
+      </main>
 
   );
   
