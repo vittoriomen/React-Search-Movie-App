dca8ee912d3040e905e1fad3efb263804deb2891
vittorio <vittoriomenicanti@hotmail.com>
2025-02-08 17:38:34 +0100

    Favorites component set up and react router installed

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,14 @@
+
+function Favorites(){
+
+  return(
+    <div className="favorites-empty">
+    
+      <h2>No Favorites Movies Yet</h2>
+      <p>Start adding movies to your  favorites and they will appear  here</p>
+    </div>
+  )
+
+}
+
+export default Favorites
